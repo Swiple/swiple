06467d35a5922badf270154a43d100c9f91f1a5d
Kenton Parton <kknoxparton@gmail.com>
2022-06-24 00:39:56 +0100

    use schedules tag

--- backend/app/api/api_v1/scheduler_router.py
+++ backend/app/api/api_v1/scheduler_router.py
@@ -2,4 +2,4 @@ from app.api.api_v1.endpoints import scheduler
 from fastapi import APIRouter
 
 router = APIRouter()
-router.include_router(scheduler.router, prefix="/schedules", tags=["Scheduler"])
+router.include_router(scheduler.router, prefix="/schedules", tags=["Schedules"])
