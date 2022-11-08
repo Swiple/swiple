c089fdae941019f5a35f55be6a44d27e71ad9d2d
Kenton Parton <kknoxparton@gmail.com>
2022-11-08 11:45:00 +0000

    Add BigQuery to ReadMe and Docs

--- docs/src/components/SupportedDatabases/index.js
+++ docs/src/components/SupportedDatabases/index.js
@@ -4,56 +4,60 @@ import styles from './styles.module.css';
 
 const FeatureList = [
   {
-      title: 'PostgreSQL',
-        Svg: require('@site/static/img/databases/postgresql.png').default,
-      //   height: 200,
-      // width: 200,
-      height: 100,
-        width: 100,
-    },
-    {
-        title: 'Snowflake',
-        Svg: require('@site/static/img/databases/snowflake-icon.png').default,
-        // height: 200,
-        // width: 200,
-        height: 100,
-        width: 100,
-    },
-    {
-        title: 'Redshift',
-        Svg: require('@site/static/img/databases/redshift.png').default,
-        height: 100,
-        width: 100,
-    },
-    {
-        title: 'Athena',
-        Svg: require('@site/static/img/databases/athena.png').default,
-        height: 100,
-        width: 100,
-    },
-    {
-        title: 'MySQL',
-        Svg: require('@site/static/img/databases/mysql.png').default,
-        height: 180,
-        width: 180,
-    },
-    {
-        title: 'Trino',
-        Svg: require('@site/static/img/databases/trino.png').default,
-        height: 150,
-        width: 150,
-    },
+    title: 'Snowflake',
+    Svg: require('@site/static/img/databases/snowflake-icon.png').default,
+    height: 100,
+    width: 100,
+  },
+  {
+    title: 'BigQuery',
+    Svg: require('@site/static/img/databases/bigquery-icon.png').default,
+    height: 100,
+    width: 100,
+  },
+  {
+    title: 'Redshift',
+    Svg: require('@site/static/img/databases/redshift.png').default,
+    height: 100,
+    width: 100,
+  },
+  {
+    title: 'PostgreSQL',
+    Svg: require('@site/static/img/databases/postgresql.png').default,
+    height: 100,
+    width: 100,
+  },
+  {
+    title: 'Trino',
+    Svg: require('@site/static/img/databases/trino.png').default,
+    height: 150,
+    width: 150,
+  },
+  {
+    title: 'Athena',
+    Svg: require('@site/static/img/databases/athena.png').default,
+    height: 100,
+    width: 100,
+  },
+  {
+    title: 'MySQL',
+    Svg: require('@site/static/img/databases/mysql.png').default,
+    height: 180,
+    width: 180,
+  },
+
+
 ];
 
 function Feature({Svg, title, height, width}) {
   return (
-    <div className={clsx('col col--4 margin-bottom--lg')}>
+    <div className={clsx('col col--3 margin-bottom--lg')}>
       <div className={`text--center ${styles.centeredImg}`}>
         <img
-            src={Svg}
-            style={{ height, width }}
-            className={styles.featureSvg}
-            role="img"
+          src={Svg}
+          style={{height, width}}
+          className={styles.featureSvg}
+          role="img"
         />
       </div>
       <div className="text--center padding-horiz--md">
@@ -68,7 +72,7 @@ export default function SupportedDatabases() {
   return (
     <section className={styles.features}>
       <div className="container">
-          <h1>Supported Databases</h1>
+        <h1>Supported Databases</h1>
         <div className="row">
           {FeatureList.map((props, idx) => (
             <Feature key={idx} {...props} />
