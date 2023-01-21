2e1bef7d114e0ccb74ea2cef415b5e61af150b81
Kenton Parton <kknoxparton@gmail.com>
2023-01-21 22:27:40 +0000

    Update create_admin_user.py

--- backend/app/scripts/create_admin_user.py
+++ backend/app/scripts/create_admin_user.py
@@ -7,35 +7,36 @@ import asyncio
 
 
 async def create_admin_user():
-    user_db = OpenSearchUserDatabase(UserDB, async_client)
-    user_manager = UserManager(user_db)
-    user = await user_manager.get_by_email(settings.ADMIN_EMAIL)
+    async with async_client:
+        user_db = OpenSearchUserDatabase(UserDB, async_client)
+        user_manager = UserManager(user_db)
+        user = await user_manager.get_by_email(settings.ADMIN_EMAIL)
 
-    if user is None:
-        print("Creating admin user.")
-        # Create admin user
-        await user_manager.create(
-            UserCreate(
+        if user is None:
+            print("Creating admin user.")
+            # Create admin user
+            await user_manager.create(
+                UserCreate(
+                    email=settings.ADMIN_EMAIL,
+                    password=settings.ADMIN_PASSWORD,
+                    is_superuser=True,
+                    is_verified=True,
+                )
+            )
+            print("Admin user created")
+        else:
+            print("Admin user already exists. Updating...")
+            user_update = UserUpdate(
                 email=settings.ADMIN_EMAIL,
                 password=settings.ADMIN_PASSWORD,
                 is_superuser=True,
-                is_verified=True,
+                is_active=True,
             )
-        )
-        print("Admin user created")
-    else:
-        print("Admin user already exists. Updating...")
-        user_update = UserUpdate(
-            email=settings.ADMIN_EMAIL,
-            password=settings.ADMIN_PASSWORD,
-            is_superuser=True,
-            is_active=True,
-        )
-        await user_manager.update(
-            user_update=user_update,
-            user=user,
-        )
-        print("Admin user updated.")
+            await user_manager.update(
+                user_update=user_update,
+                user=user,
+            )
+            print("Admin user updated.")
 
 
 asyncio.run(create_admin_user())
