d5dc4cc180f5eb713532c90b648212485881543c
Kenton Parton <kknoxparton@gmail.com>
2023-01-31 23:23:09 +0000

    Add /api/v1/health endpoint (#72)
    * Add /api/v1/health endpoint
    
    * add/ health test coverage

--- backend/app/api/api_v1/scheduler_router.py
+++ backend/app/api/api_v1/scheduler_router.py
@@ -1,5 +1,6 @@
-from app.api.api_v1.endpoints import scheduler
+from app.api.api_v1.endpoints import health, scheduler
 from fastapi import APIRouter
 
 router = APIRouter()
+router.include_router(health.router, tags=["Health"])
 router.include_router(scheduler.router, prefix="/schedules", tags=["Schedules"])
