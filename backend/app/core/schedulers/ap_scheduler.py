887a3e9f92f90ff9e8b5265d5f4d88d6a322663b
Kenton Parton <kknoxparton@gmail.com>
2023-03-26 13:33:42 +0100

    Update ap_scheduler to use celery

--- backend/app/core/schedulers/ap_scheduler.py
+++ backend/app/core/schedulers/ap_scheduler.py
@@ -1,7 +1,5 @@
 from typing import List, Dict
 
-from app.tasks.validate import scheduler_trigger_validation
-from app.core.runner import run_dataset_validation
 from app.core.schedulers.scheduler_interface import SchedulerInterface
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
 from apscheduler.triggers.interval import IntervalTrigger
@@ -13,7 +11,7 @@ from apscheduler.job import Job
 from pytz import utc
 from app.settings import settings
 import app.constants as c
-
+from app.worker.tasks.validation import run_validation
 from app.models.schedule import Schedule
 import uuid
 import datetime
@@ -56,8 +54,7 @@ class ApScheduler(SchedulerInterface):
     def add_schedule(self, schedule: Schedule, datasource_id: str, dataset_id: str):
         return self.ap_scheduler.add_job(
             id=f"{datasource_id}__{dataset_id}__{uuid.uuid4()}",
-            # func=scheduler_trigger_validation,
-            func=run_dataset_validation,
+            func=run_validation.delay,
             kwargs={"dataset_id": dataset_id},
             misfire_grace_time=schedule.misfire_grace_time,
             max_instances=schedule.max_instances,
