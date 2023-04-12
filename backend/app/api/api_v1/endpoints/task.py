7a4fef8372e7feac190f58905d065ee253d8aa55
Kenton Parton <kknoxparton@gmail.com>
2023-04-12 13:09:43 +0100

    Remove task ready endpoint

--- backend/app/api/api_v1/endpoints/task.py
+++ backend/app/api/api_v1/endpoints/task.py
@@ -1,9 +1,8 @@
 from fastapi import APIRouter
 from fastapi.params import Depends
 from app.core.users import current_active_user
-from app.models.task import TaskReadyResponse, TaskResultResponse
+from app.models.task import TaskResultResponse
 from app.repositories.task import TaskRepository, get_task_repository
-from app.worker.app import celery_app
 
 
 router = APIRouter(
@@ -17,9 +16,3 @@ def get_task(
     repository: TaskRepository = Depends(get_task_repository),
 ):
     return repository.get(task_id)
-
-
-@router.get("/{task_id}/ready", response_model=TaskReadyResponse)
-def get_task_ready(task_id: str):
-    is_ready = celery_app.AsyncResult(task_id).ready()
-    return {"is_ready": is_ready}
