af88ffe4a5555c3a0ae05544995a73b911567543
Kenton Parton <kknoxparton@gmail.com>
2023-04-13 17:06:29 +0100

    Forbid delete or update of admin user

--- backend/app/scripts/create_admin_user.py
+++ backend/app/scripts/create_admin_user.py
@@ -2,7 +2,7 @@ import fastapi_users.manager
 
 from app.settings import settings
 from app.core.users import get_user_db, get_user_manager
-from app.models.users import UserUpdate, UserCreate
+from app.models.users import AdminUserCreate, AdminUserUpdate
 import asyncio
 
 
@@ -13,26 +13,29 @@ async def create_admin_user():
         user = await user_manager.get_by_email(settings.ADMIN_EMAIL)
 
         print("Admin user already exists. Updating...")
-        user_update = UserUpdate(
+        user_update = AdminUserUpdate(
             email=settings.ADMIN_EMAIL,
             password=settings.ADMIN_PASSWORD,
             is_superuser=True,
             is_active=True,
+            is_admin_user=True,
         )
         await user_manager.update(
             user_update=user_update,
             user=user,
+            allow_admin_update=True,
         )
         print("Admin user updated.")
     except fastapi_users.manager.UserNotExists:
         print("Creating admin user.")
         # Create admin user
         await user_manager.create(
-            UserCreate(
+            AdminUserCreate(
                 email=settings.ADMIN_EMAIL,
                 password=settings.ADMIN_PASSWORD,
                 is_superuser=True,
                 is_verified=True,
+                is_admin_user=True,
             )
         )
         print("Admin user created")
